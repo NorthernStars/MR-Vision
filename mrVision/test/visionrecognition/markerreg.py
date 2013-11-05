a4bb61f45ed527791be684145604747d4ccbcbb9
northernstars <mail@northern-stars.de>
2013-11-05 17:34:51 +0100

    Created other variant of marker detection algorithm.

--- mrVision/test/visionrecognition/markerreg.py
+++ mrVision/test/visionrecognition/markerreg.py
@@ -1,6 +1,55 @@
 from cv2 import *
 from copy import copy
 from os.path import isfile
+from math import sqrt, atan2
+from operator import itemgetter
+from numpy import array
+
+def sortContour(contour=[]):
+    '''
+    Sorts contour clockwise
+    @param contour: Contour with 4 points
+    @return: Contour and list of vectors with clockwise sorted vectors
+    '''
+    if len(contour) != 4:
+        return contour
+    
+    # create list of polar coordinates
+    polar = []
+    for p in contour:
+        if len(p[0]) == 2:
+            
+            # get cartesian coordinates
+            x = p[0][0]
+            y = p[0][1]
+            
+            # calc polar coordinates
+            r = sqrt(x*x + y*y)
+            a = atan2(y, x)
+            
+            # add polar point
+            polar.append( [a, r, x, y] )
+    
+    # get nearest and farest point
+    polar = sorted(polar, key=itemgetter(1))
+    p0 = polar[0]
+    p2 = polar[-1]
+    del polar[0]
+    del polar[-1]
+    
+    # get other points by angle
+    polar = sorted(polar, key=itemgetter(0))
+    p1 = polar[0]
+    p3 = polar[1]
+    
+    # create cartesian coordinates
+    p0 = [p0[2], p0[3]]
+    p1 = [p1[2], p1[3]]
+    p2 = [p2[2], p2[3]]
+    p3 = [p3[2], p3[3]]
+    
+    return contour, [p0, p1, p2, p3]
+    
 
 def minMax(contour=[]):
     '''
@@ -69,28 +118,110 @@ def regMarker(fName=""):
     # read image
     img = imread(fName)
     
-    shapeList = []
+    shapeList = [[], [], [], []]
     
     # convert to gray and blur
     gray = cvtColor(img, COLOR_RGB2GRAY) 
                 
     # create binary image
-    gray = threshold( gray, 30, 255, THRESH_BINARY )[1]        
+    gray = threshold( gray, 30, 255, THRESH_BINARY )[1]
+    center = (gray.shape[1]*0.5, gray.shape[0]*0.5)
     
-    # find contours
-    contours, hirachy = findContours( gray, RETR_TREE, CHAIN_APPROX_SIMPLE )
+    for i in range(4):
+        angle = i*90
+        rot = getRotationMatrix2D(center, angle, 1.0)
+        gray = warpAffine(gray, rot, gray.shape)  
     
-    imgArea = gray.shape[0]*gray.shape[1]    
-    for i in range( len(contours) ):
-        cnt = contours[i]
+        # find contours
+        contours, hirachy = findContours( gray, RETR_TREE, CHAIN_APPROX_SIMPLE )
         
-        if len( cnt ) > 3:            
-            # get area of contour and arc length
-            cntArea = contourArea( cnt )
+        imgArea = gray.shape[0]*gray.shape[1]    
+        for i in range( len(contours) ):
+            cnt = contours[i]
             
-            # check if contour area is big enough
-            if cntArea < 0.9*imgArea:                    
-                shapeList.append(cnt)
+            if len( cnt ) > 3:            
+                # get area of contour and arc length
+                cntArea = contourArea( cnt )
                 
+                # check if contour area is big enough
+                if cntArea < 0.9*imgArea:                    
+                    shapeList[i-1].append(cnt)
+                    
     # return list of shapes
     return shapeList
+
+
+
+def getMatch(contour, refSet):
+    # search a reference for every contour
+    y = 0
+    set = copy(refSet)
+    matches = []
+    
+    # search for best reference contour
+    # for every contour touch every reference
+    # contour and calculate match
+    matches = []
+    
+    for i in range( len(contour) ):        
+        cnt = copy( contour[i] )
+        
+        for y in range( len(refSet) ):
+            refCnt = copy( refSet[y] )
+            
+            # check number of points
+            # must be more than 3 points
+            if len(cnt) > 3 and len(refCnt) > 3:
+                # get match
+                match = matchShapes( cnt, refCnt, cv.CV_CONTOURS_MATCH_I3, 0 ) 
+                
+                # get minmax values
+                mmCnt = minMax( cnt )
+                mmRefCnt = minMax( refCnt )
+                
+                if None not in mmCnt and None not in mmRefCnt:
+                    # get distance between shapes
+                    centerCnt = (mmCnt[1]-mmCnt[0], mmCnt[3]-mmCnt[2])
+                    centerRefCnt = (mmRefCnt[1]-mmRefCnt[0], mmRefCnt[3]-mmRefCnt[2])
+                    distance = (centerCnt[0]-centerRefCnt[0], centerCnt[1]-centerRefCnt[1])
+                    distance = sqrt( distance[0]*distance[0] + distance[1]*distance[1] )
+                    
+                    # calculate current match
+                    match *= distance
+                    
+                    # add match
+                    matches.append( [i, y, match] )
+        
+    # return best match
+    return calcMatch(matches)
+
+
+def calcMatch(matches=[]):
+    '''
+    Calculates complete match
+    @param matches: List of matches
+    @return: Match value 
+    '''
+    retMatch = -1
+    count = 0
+    excCnt = []
+    excRef = []
+    
+    # search through all matches
+    for match in matches:        
+        if match[0] not in excCnt and match[1] not in excRef:
+            excCnt.append(match[0])
+            excRef.append(match[1])
+            count += 1
+            
+            if retMatch == -1:
+                retMatch = match[2]
+            else:
+                retMatch += match[2]
+    
+    if retMatch != -1 and count > 0:
+        retMatch /= count;
+        
+    return retMatch
+        
+    
--- mrVision/test/visionrecognition/reg.py
+++ mrVision/test/visionrecognition/reg.py
@@ -1,8 +1,8 @@
 from cv2 import *
 from copy import copy
 import numpy as np
-from markerreg import regMarker, minMax, getContours
-from math import sqrt
+from markerreg import regMarker, minMax, getContours, sortContour, getMatch
+from math import sqrt, acos, degrees, ceil
 from operator import itemgetter
 
 contourAreaMin = 0.001
@@ -12,15 +12,15 @@ contourThreshold = 0.4
 
 th1 = 100
 th2 = 255
-blur = 1
+blur = 3
 
-refRange = 10
+refRange = 2
 testfile = "test2.jpg"
 
 epsilon1 = 0.05
 epsilon2 = 0.02
 
-usecam = True
+usecam = False
 
 
 # load id 1 contours
@@ -51,14 +51,14 @@ else:
     cam = VideoCapture(0)
     retval = False
     while not retval:
-        retval, img = cam.read()
-        if retval:
-            imshow("source", img)
+        retval, img = cam.read()    
     
-    
-
-while waitKey(1) < 0:
-    retval, img = cam.read()
+k = 0
+#while waitKey(1) < 0:
+while k == 0:
+    k = 1
+    #retval, img = cam.read()
+    retval = True
     
     if retval:
         # convert to gray and blur
@@ -87,25 +87,29 @@ while waitKey(1) < 0:
         drawApproxCnts = []
         drawHulls = []
         
-        for cnt in contours:
+        # search through found contours
+        for markerCnt in contours:
             
-            if len( cnt ) == 4:
-                
+            if len( markerCnt ) == 4:
                 # get area of contour
-                cntArea = contourArea( cnt )        
+                cntArea = contourArea( markerCnt )        
                 
                 # check if contour area is big enough
                 if cntArea > imgArea*contourAreaMin and cntArea < imgArea*contourAreaMax:
                     # show contour data
-        #             print "\n----------------\nMARKER"
-        #             print "\tarea", cntArea
+                    print "\n----------------\nMARKER"
+                    print "\tarea:", cntArea
+                    print "\tcontour:\n", markerCnt
+                    
+                    # get contour points in clockwise order
+                    markerCnt, vec = sortContour(markerCnt)
         
                     # draw contour
-                    drawContours(dstC, [cnt], -1, (0,0,255))
-                    imshow("contours", dstC)
+                    drawContours(dstC, [markerCnt], -1, (0,0,255))
+                    
                     
                     # slice image
-                    minX, maxX, minY, maxY = minMax(cnt)
+                    minX, maxX, minY, maxY = minMax(markerCnt)
                     
                     if None in [minX, maxX, minY, maxY]:
                         continue
@@ -113,92 +117,103 @@ while waitKey(1) < 0:
                     slice = gray[minY+contourPadding:maxY-contourPadding, minX+contourPadding:maxX-contourPadding]
                     slice = resize( slice, (100,100) )
                     
-        #             slice2 = img[minY+contourPadding:maxY-contourPadding, minX+contourPadding:maxX-contourPadding]
-        #             slice2 = resize( slice2, (100,100) )
+                    slice2 = img[minY+contourPadding:maxY-contourPadding, minX+contourPadding:maxX-contourPadding]
+                    slice2 = resize( slice2, (100,100) )
+                    
+                    # rotate image
+                    v0 = (vec[0][0], vec[0][1])
+                    v1 = (vec[1][0], vec[1][1])
+                    v = (v1[0]-v0[0], v1[1]-v0[1])
+                    angle = 90-degrees( acos( v[1]/sqrt(v[0]*v[0] + v[1]*v[1]) ) )
+                    
+                    m = moments(markerCnt)
+                    center = ( slice.shape[1]*0.5, slice.shape[0]*0.5 )
+                    
+                    rot = getRotationMatrix2D(center, angle, 1.0)
+                    slice = warpAffine(slice, rot, slice.shape)
+                    slice2 = warpAffine(slice2, rot, slice.shape)
+                    
+                    line(dstC, v0, v1, (0,255,0), 2)
+                    line(dstC, (0,0), (0,100), (0,255,0), 2)
+                    
+                    imshow("contours", dstC)
+                    
                     
                     # find contours on marker
-                    sContours = getContours( img=slice, epsilon=epsilon2, retr=RETR_LIST )
+                    markerIdContours = getContours( img=slice, epsilon=epsilon2, retr=RETR_LIST )
                     
                     # get ID
-                    found = False
                     idList = []
-                    for key in idContours:
-                        set = copy(idContours[key])
-                        matches = []
+                    
+                    # search through all available markers
+                    for idKey in idContours:
+                        rotationSet = copy( idContours[idKey] )
+                        rotationMatch = -1
                         
-                        # search a reference for every contour
-                        y = 0
-                        for c in sContours:
-                            i = 0                                      
+                        print "------------------------"
+                        print "MARKER ID", idKey
+                        
+                        # search through all available rotations
+                        for i in range(len(rotationSet)):
+                            set = copy( rotationSet[i] )
                             
-                            while i < len(set):   
-                                refCnt = set[i]   
-                                match = None                     
-                                    
-                                if len(c) > 2 and len(refCnt) > 2:
-                                    # get shape matches        
-                                    match = matchShapes( c, refCnt, cv.CV_CONTOURS_MATCH_I3, 0 )
-                                    
-                                    # show match data
-        #                             print "\nmatch with key", key, ":(", i, ",", y, ") =",  match
-        #                             dst = copy(slice2)
-        #                             drawContours(dst, [refCnt], -1, (0,0,255))
-        #                             drawContours(dst, [c], -1, (255,0,0))
-        #                             imshow("match", dst)
-                                    
-                                    
-                                # check if contours match            
-                                if match != None and match < contourThreshold:
-                                    # remove shape                            
-                                    del set[i]
-                                    
-                                    # calculate distance between shapes
-                                    try:
-                                        mC = moments(c)
-                                        mRef = moments(refCnt)
-                                        centerC = ( int(mC['m10']/mC['m00']), int(mC['m01']/mC['m00']) )
-                                        centerRef = ( int(mRef['m10']/mRef['m00']), int(mRef['m01']/mRef['m00']) )
-                                        dst = (centerC[0]-centerRef[0], centerC[1]-centerRef[1])
-                                        dst = sqrt( dst[0]*dst[0] + dst[1]*dst[1] )
-                                        
-                                        # add shape to list                            
-                                        matches.append( [match, dst] )
-                                        
-                                        # show additional match data
-            #                             print "distance", dst 
-            #                             print "\tmatched"
-            #                             waitKey(0)
-                                        break;
-                                    except:
-                                        pass
-                                else:
-                                    i += 1
-        
+                            print "------------------------"
+                            print "ROTATION", i*90
+                            
+                            # get and add match
+                            match = getMatch(markerIdContours, set)                            
+                            if (match < rotationMatch or rotationMatch == -1) and match >= 0:
+                                rotationMatch = match
                                 
-                            y += 1
+                        # add best match to list
+                        if rotationMatch >= 0:
+                            idList.append( [idKey, match, i*90] )
+                    
+                    # sort idList
+                    idList = sorted(idList, key=itemgetter(1))
+                    print "\nidList:"
+                    for e in idList:
+                        e[2] += angle
+                        print e
                         
-                        # check if a reference for all contours could be found
-                        if len(matches) > 0 and len(set) == 0:
-                            # calculate overall match
-                            match = 0
-                            for m in matches:
-                                match += m[0]*m[1]
-                                match *= 1.0/len(matches)
-                            idList.append( [key, match, 1.0/len(matches)] )
                         
                     
-                    # sort list of recognized marker ids
-                    if len(idList) > 0 and len(cnt) == 4:
-                        idList = sorted(idList, key=itemgetter(1, 2))
-                        cnt = convexHull(cnt, clockwise=True)
-                        
-                        if len(cnt) == 4:                        
-                            # get vectors
-                            v0 = cnt[0][0]
-                            v1 = cnt[1][0]
-                            v3 = cnt[3][0]
-                                        
-                        print "\n-------------------------"
-                        print "marker list", idList
-                        print "Detected marker", idList[0]
-                        print "marker-contour\nv0", v0, "v1", v1, "v3", v3
+                    
+#                     for key in idContours:
+#                         rotSet = copy(idContours[key])
+#                         rotMatches = []
+#                         
+#                         # iterate over all rotations of marker id
+#                         for s in rotSet: 
+#                             set = copy(s)
+#                             matches = [] 
+#                             print "set length", len(s)
+#                             
+#                             getMatch()
+#                             
+#                             # check if a reference for all contours could be found
+#                             print "lmatch", len(matches), "lset", len(set)
+#                             if len(matches) > 0 and len(set) == 0:
+#                                 # calculate overall match
+#                                 match = 0
+#                                 for m in matches:
+#                                     match += m[0]*m[1]
+#                                     match *= 1.0/len(matches)
+#                                 idList.append( [key, match, 1.0/len(matches)] )
+#                         
+#                     
+#                     # sort list of recognized marker ids
+#                     if len(idList) > 0 and len(cnt) == 4:
+#                         idList = sorted(idList, key=itemgetter(1, 2))
+#                         cnt = convexHull(cnt, clockwise=True)
+#                         
+#                         if len(cnt) == 4:                        
+#                             # get vectors
+#                             v0 = cnt[0][0]
+#                             v1 = cnt[1][0]
+#                             v3 = cnt[3][0]
+#                                         
+#                         print "\n-------------------------"
+#                         print "marker list", idList
+#                         print "Detected marker", idList[0]
+#                         print "marker-contour\nv0", v0, "v1", v1, "v3", v3
